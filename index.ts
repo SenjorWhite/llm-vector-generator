6b7240819f9787d27e31558c8348803cada47c3c
Far Home <senjorwhite@gmail.com>
2024-02-27 15:27:50 -0700

    Added embedding conversion function

--- index.ts
+++ index.ts
@@ -1,4 +1,5 @@
 import { exit } from 'process';
+import OpenAI from 'openai';
 import * as path from 'path';
 import * as fs from 'fs';
 import 'dotenv/config';
@@ -7,29 +8,54 @@ import { convert } from 'html-to-text';
 const argv = require('minimist')(process.argv.slice(2));
 const action = argv._[0];
 const sourceFile = argv._[1];
+const openai = new OpenAI({ apiKey: process.env.OPENAI_API_KEY });
 
-if (!action || !sourceFile) {
-	// TODO: Hint
-	exit();
-}
+main();
 
-const articles = readJsonFile(sourceFile);
-if (!articles) {
-	// TODO: Hint
-	exit();
-}
-
-if (action === 'html2text') {
-	const outputFile = argv._[2] || `${path.parse(sourceFile).name}-H2T-${Date.now()}.json`;
-	try {
-		articles.forEach((article) => {
-			article.text = convert(article.html);
-		});
-
-		fs.writeFileSync(outputFile, JSON.stringify(articles));
-	} catch (e) {
+async function main() {
+	if (!action || !sourceFile) {
 		// TODO: Hint
-		console.error(e);
+		exit();
+	}
+
+	const articles = readJsonFile(sourceFile);
+	if (!articles) {
+		// TODO: Hint
+		exit();
+	}
+
+	if (action === 'html2text') {
+		const outputFile = argv._[2] || `${path.parse(sourceFile).name}-H2T-${Date.now()}.json`;
+		try {
+			articles.forEach((article) => {
+				article.text = convert(article.html);
+			});
+
+			fs.writeFileSync(outputFile, JSON.stringify(articles));
+		} catch (e) {
+			// TODO: Hint
+			console.error(e);
+		}
+	} else if (action === 'embedding') {
+		const outputFile = argv._[2] || `EMBEDDING-${Date.now()}.lvg.json`;
+		try {
+			// TODO: Customizable Field Names
+			for (let i = 0; i < articles.length; i++) {
+				if (articles[i].text) {
+					const embedding = await getEmbedding(articles[i].text);
+					articles[i].text_vector = embedding;
+
+					// TODO: Remove
+					console.log(articles[i].title);
+					console.log(embedding);
+				}
+			}
+
+			fs.writeFileSync(outputFile, JSON.stringify(articles));
+		} catch (e) {
+			// TODO: Hint
+			console.error(e);
+		}
 	}
 }
 
@@ -42,4 +68,14 @@ function readJsonFile(filePath: string): [any] | null {
 		console.error(e);
 		return null;
 	}
+}
+
+async function getEmbedding(text: string) {
+	const embedding = await openai.embeddings.create({
+		model: 'text-embedding-ada-002',
+		input: text,
+		encoding_format: 'float',
+	});
+
+	return embedding.data[0].embedding;
 }
