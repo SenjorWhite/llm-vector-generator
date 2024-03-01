ed400614cf875004d77e75265d58f1abc784e302
Far Home <senjorwhite@gmail.com>
2024-03-01 01:04:06 -0700

    Added openai question generating and the corresponding embedding and search

--- db-handler.ts
+++ db-handler.ts
@@ -1,34 +1,68 @@
 import { Collection, MongoClient } from 'mongodb';
 import 'dotenv/config';
+import OpenAI from 'openai';
 
 import * as fs from 'fs';
 import * as path from 'path';
 
 const argv = require('minimist')(process.argv.slice(2));
 const action = argv._[0];
-const sourceFile = argv._[1];
 
 const uri = process.env.MONGODB_URI as string;
 const databaseName = process.env.MONGODB_DATABASE as string;
 const collectionName = process.env.MONGODB_COLLECTION as string;
+
+const openai = new OpenAI({ apiKey: process.env.OPENAI_API_KEY });
 
 // TODO: make this as an importable function
 
 async function main() {
 	const client = new MongoClient(uri);
 	try {
-		const documents = getVectorDocuments();
-
-		console.log(documents);
-
 		await client.connect();
 		console.log('Connected to MongoDB');
+		const sourceFile = argv._[1];
 
 		const database = client.db(databaseName);
 		const collection = database.collection(collectionName);
 
-		const result = await collection.insertMany(documents);
-		console.log(`Inserted ${result.insertedCount} documents into collection`);
+		if (action === 'add') {
+			const documents = getVectorDocuments(sourceFile);
+			const result = await collection.insertMany(documents);
+			console.log(`Inserted ${result.insertedCount} documents into collection`);
+		} else if (action === 'search') {
+			// TODO: Hint
+			// argv._[1] required
+			const context = argv._[1];
+			const embedding = await getEmbedding(context);
+			// console.log(embedding);
+
+			const agg = [
+				{
+					$vectorSearch: {
+						index: 'vector_index',
+						path: 'text_embedding',
+						queryVector: embedding,
+						numCandidates: 10,
+						limit: 5,
+					},
+				},
+				{
+					$project: {
+						_id: 0,
+						id: 1,
+						title: 1,
+						question: 1,
+						score: {
+							$meta: 'vectorSearchScore',
+						},
+					},
+				},
+			];
+
+			const result = collection.aggregate(agg);
+			await result.forEach((doc) => console.dir(JSON.stringify(doc)));
+		}
 	} catch (e) {
 		console.error('Error:', e);
 	} finally {
@@ -37,14 +71,32 @@ async function main() {
 	}
 }
 
-function getVectorDocuments() {
-	const articles = readJsonFile(sourceFile);
+function getVectorDocuments(filePath: string) {
+	const articles = readJsonFile(filePath);
 	const documents: any[] = [];
 
 	articles?.forEach((article) => {
-		const metadata: Record<string, any> = { title: article.title, id: article.id };
-		const doc = { vector: article.text_vector, metadata: metadata };
+		const doc = {
+			text_embedding: article.text_vector,
+			title: article.title,
+			id: article.id,
+			question: 'article-context',
+		};
 		documents.push(doc);
+
+		if (article.questions) {
+			const questions = article.questions;
+			const questionVectors = article.question_vectors;
+			for (let i = 0; i < questions.length; i++) {
+				const doc = {
+					text_embedding: questionVectors[i],
+					title: article.title,
+					id: article.id,
+					question: questions[i],
+				};
+				documents.push(doc);
+			}
+		}
 	});
 
 	return documents;
@@ -60,6 +112,17 @@ function readJsonFile(filePath: string): [any] | null {
 		console.error(e);
 		return null;
 	}
+}
+
+// TODO: DRY
+async function getEmbedding(text: string) {
+	const embedding = await openai.embeddings.create({
+		model: 'text-embedding-ada-002',
+		input: text,
+		encoding_format: 'float',
+	});
+
+	return embedding.data[0].embedding;
 }
 
 main();
--- index.ts
+++ index.ts
@@ -45,9 +45,36 @@ async function main() {
 					const embedding = await getEmbedding(articles[i].text);
 					articles[i].text_vector = embedding;
 
-					// TODO: Remove
+					if (articles[i].questions) {
+						const questions = articles[i].questions;
+						articles[i].question_vectors = [];
+						for (let j = 0; j < questions.length; j++) {
+							const embedding = await getEmbedding(questions[j]);
+							articles[i].question_vectors[j] = embedding;
+
+							console.log(questions[j]);
+						}
+					}
+				}
+			}
+
+			fs.writeFileSync(outputFile, JSON.stringify(articles));
+		} catch (e) {
+			// TODO: Hint
+			console.error(e);
+		}
+	} else if (action === 'questions') {
+		const outputFile = argv._[2] || `QUESTIONS-${Date.now()}.lvg.json`;
+
+		try {
+			// TODO: Customizable Field Names
+			for (let i = 0; i < articles.length; i++) {
+				if (articles[i].text) {
+					const questions = await getQuestions(articles[i].text);
+					articles[i].questions = questions;
+
 					console.log(articles[i].title);
-					console.log(embedding);
+					console.log(questions);
 				}
 			}
 
@@ -78,4 +105,25 @@ async function getEmbedding(text: string) {
 	});
 
 	return embedding.data[0].embedding;
+}
+
+async function getQuestions(text: string) {
+	const completion = await openai.chat.completions.create({
+		messages: [
+			{
+				role: 'system',
+				content: `Please design questions which can full cover this article, the format should be ["question_1","question_2", ...]:
+				${text} `,
+			},
+		],
+		model: 'gpt-3.5-turbo-0125',
+	});
+
+	console.log(completion.choices[0]);
+	try {
+		const questions = JSON.parse(completion.choices[0].message.content as string);
+		return questions;
+	} catch (e) {
+		return null;
+	}
 }
